4d2c620bc515af35fd5179ec63c23a88ac510f94
RussellPeele <53414136+RussellPeele@users.noreply.github.com>
2023-11-15 13:22:17 -0500

    Update vanDerPolwradiip.jl
    ERROR: MethodError: no method matching Multiplication(::Int64)

--- vanDerPolwradiip.jl
+++ vanDerPolwradiip.jl
@@ -1,7 +1,6 @@
 #Existence of Periodic orbit in van der Pol, proof: 
 # x' = y
 # y' = mu(1-x^2)y -x
-# I think everything is correct so far! 
 
 using RadiiPolynomial
 
@@ -53,3 +52,51 @@ function DF!(DF, x, mu)
 
     return DF
 end
+
+
+
+mu = .1
+
+n = 400
+
+x̄ = Sequence(ParameterSpace() × Fourier(n, 1.0)^2, zeros(ComplexF64, 1+2*(2n+1)))
+x̄[1] = 1.0 # γ, i.e. approximate inverse of the frequency
+component(component(x̄, 2), 1)[0:14] =
+    [ 0.0247 + 0.0000im
+    0.6791 + 0.7322im
+   -0.0107 - 0.0192im
+    0.0026 - 0.0013im
+   -0.0042 - 0.0083im
+   -0.0031 - 0.0060im
+   -0.0029 - 0.0050im
+   -0.0026 - 0.0042im
+   -0.0024 - 0.0036im
+   -0.0024 - 0.0031im
+   -0.0023 - 0.0027im
+   -0.0022 - 0.0023im
+   -0.0022 - 0.0021im
+   -0.0021 - 0.0018im
+   -0.0021 - 0.0016im]
+component(component(x̄, 2), 2)[0:14] =
+    [0.0316 + 0.0000im
+    0.7491 - 0.6674im
+   -0.0062 + 0.0170im
+    0.0275 - 0.0134im
+   -0.0017 + 0.0087im
+    0.0010 + 0.0056im
+    0.0005 + 0.0051im
+    0.0009 + 0.0042im
+    0.0011 + 0.0035im
+    0.0013 + 0.0030im
+    0.0014 + 0.0026im
+    0.0015 + 0.0023im
+    0.0015 + 0.0020im
+    0.0016 + 0.0018im
+    0.0016 + 0.0016im]
+
+component(component(x̄, 2), 1)[-14:-1] .= conj.(component(component(x̄, 2), 1)[14:-1:1])
+component(component(x̄, 2), 2)[-14:-1] .= conj.(component(component(x̄, 2), 2)[14:-1:1])
+
+
+newton!((F, DF, x) -> (F!(F, x, mu), DF!(DF, x, mu)), x̄)
+
